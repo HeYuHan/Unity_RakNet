7bab321730647ad2f7ccf09679c4db6d67a9d3ad
HeYuHan <435481457@qq.com>
2017-10-31 21:56:41 +0800

    测试通过

---
+++ Assets/Script/NetworkConnection.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RakNet;
+public enum ConnectionType
+{
+    TCP = 0, UDP = 1
+}
+public interface IConnection
+{
+    int Read(byte[] data, int offset, int size);
+    int Send(byte[] data, int offset, int size);
+    void Send(byte[] data, int size);
+    ConnectionType Type { get; }
+}
+
+public class UdpConnection : IConnection
+{
+    RakPeerInterface m_Socket;
+    SystemAddress m_SystemAddress;
+    Packet m_MessagePacket;
+    public NetworkStream m_Stream;
+    public Action m_OnConnected;
+    public Action m_OnDisconnected;
+
+    ConnectionType IConnection.Type
+    {
+        get
+        {
+            return ConnectionType.UDP;
+        }
+    }
+
+    byte GetPacketIdentifier(Packet p)
+    {
+        if (p == null)
+            return 255;
+
+        if (p.data[0] == (byte)DefaultMessageIDTypes.ID_TIMESTAMP)
+        {
+            return p.data[sizeof(byte) + sizeof(ulong)];
+        }
+        else
+            return p.data[0];
+    }
+    protected void OnConnected()
+    {
+        if (m_OnConnected != null) m_OnConnected();
+    }
+    protected void OnDisconnected()
+    {
+        if (m_OnDisconnected != null) m_OnDisconnected();
+    }
+    public void Update()
+    {
+        if (null == m_Socket) return;
+        for (m_MessagePacket = m_Socket.Receive(); null != m_MessagePacket; m_Socket.DeallocatePacket(m_MessagePacket), m_MessagePacket = m_Socket.Receive())
+        {
+            // We got a packet, get the identifier with our handy function
+            DefaultMessageIDTypes packetIdentifier = (DefaultMessageIDTypes)GetPacketIdentifier(m_MessagePacket);
+
+            // Check if this is a network message packet
+            switch (packetIdentifier)
+            {
+                case DefaultMessageIDTypes.ID_DISCONNECTION_NOTIFICATION:
+                    // Connection lost normally
+                    //printf("ID_DISCONNECTION_NOTIFICATION\n");
+                    OnDisconnected();
+                    break;
+                case DefaultMessageIDTypes.ID_ALREADY_CONNECTED:
+                    // Connection lost normally
+                    //printf("ID_ALREADY_CONNECTED with guid %" PRINTF_64_BIT_MODIFIER "u\n", m_MessagePacket->guid);
+                    break;
+                case DefaultMessageIDTypes.ID_INCOMPATIBLE_PROTOCOL_VERSION:
+                    //printf("ID_INCOMPATIBLE_PROTOCOL_VERSION\n");
+                    break;
+                case DefaultMessageIDTypes.ID_REMOTE_DISCONNECTION_NOTIFICATION: // Server telling the clients of another client disconnecting gracefully.  You can manually broadcast this in a peer to peer enviroment if you want.
+                    //printf("ID_REMOTE_DISCONNECTION_NOTIFICATION\n");
+                    break;
+                case DefaultMessageIDTypes.ID_REMOTE_CONNECTION_LOST: // Server telling the clients of another client disconnecting forcefully.  You can manually broadcast this in a peer to peer enviroment if you want.
+                    //printf("ID_REMOTE_CONNECTION_LOST\n");
+                    OnDisconnected();
+                    break;
+                case DefaultMessageIDTypes.ID_REMOTE_NEW_INCOMING_CONNECTION: // Server telling the clients of another client connecting.  You can manually broadcast this in a peer to peer enviroment if you want.
+                    //printf("ID_REMOTE_NEW_INCOMING_CONNECTION\n");
+                    break;
+                case DefaultMessageIDTypes.ID_CONNECTION_BANNED: // Banned from this server
+                    //printf("We are banned from this server.\n");
+                    break;
+                case DefaultMessageIDTypes.ID_CONNECTION_ATTEMPT_FAILED:
+                    //printf("Connection attempt failed\n");
+                    OnDisconnected();
+                    break;
+                case DefaultMessageIDTypes.ID_NO_FREE_INCOMING_CONNECTIONS:
+                    // Sorry, the server is full.  I don't do anything here but
+                    // A real app should tell the user
+                    //printf("ID_NO_FREE_INCOMING_CONNECTIONS\n");
+                    OnDisconnected();
+                    break;
+
+                case DefaultMessageIDTypes.ID_INVALID_PASSWORD:
+                    //printf("ID_INVALID_PASSWORD\n");
+                    OnDisconnected();
+                    break;
+
+                case DefaultMessageIDTypes.ID_CONNECTION_LOST:
+                    // Couldn't deliver a reliable packet - i.e. the other system was abnormally
+                    // terminated
+                    //printf("ID_CONNECTION_LOST\n");
+                    OnDisconnected();
+                    break;
+
+                case DefaultMessageIDTypes.ID_CONNECTION_REQUEST_ACCEPTED:
+                    // This tells the client they have connected
+                    //printf("ID_CONNECTION_REQUEST_ACCEPTED to %s with GUID %s\n", m_MessagePacket->systemAddress.ToString(true), m_MessagePacket->guid.ToString());
+                    //printf("My external address is %s\n", m_Socket->GetExternalID(m_MessagePacket->systemAddress).ToString(true));
+                    m_SystemAddress = m_MessagePacket.systemAddress;
+                    OnConnected();
+                    break;
+                case DefaultMessageIDTypes.ID_CONNECTED_PING:
+                case DefaultMessageIDTypes.ID_UNCONNECTED_PING:
+                    //printf("Ping from %s\n", m_MessagePacket->systemAddress.ToString(true));
+                    break;
+                default:
+                    // It's a client, so just show the message
+                    //printf("%s\n", p->data);
+                    if (null != m_Stream) m_Stream.OnParseMessage();
+                    break;
+            }
+        }
+    }
+    public int Read(byte[] data, int offset, int size)
+    {
+        if (null != m_MessagePacket)
+        {
+            if (size < m_MessagePacket.length)
+            {
+                throw new Exception("data size too less");
+            }
+            Array.Copy(m_MessagePacket.data, 0, data, offset, m_MessagePacket.length);
+            return (int)m_MessagePacket.length;
+        }
+        return 0;
+    }
+    public void Send(byte[] data, int size)
+    {
+        uint ret = m_Socket.Send(data,size, PacketPriority.HIGH_PRIORITY, PacketReliability.RELIABLE_ORDERED, '0', m_SystemAddress, false);
+        Debug.Log(ret);
+    }
+
+
+
+    public bool Connect(string ip, int port, string pwd)
+    {
+
+        m_Socket = RakPeerInterface.GetInstance();
+        m_Socket.AllowConnectionResponseIPMigration(false);
+        SocketDescriptor socketDescriptor = new SocketDescriptor(0, "0");
+        socketDescriptor.socketFamily = 2;
+        m_Socket.Startup(8, socketDescriptor, 1);
+        m_Socket.SetOccasionalPing(true);
+        ConnectionAttemptResult car = m_Socket.Connect(ip, (ushort)port, pwd, pwd.Length);
+        if (car == ConnectionAttemptResult.CONNECTION_ATTEMPT_STARTED)
+        {
+            return true;
+        }
+        else
+        {
+            m_Socket.Shutdown(300);
+            RakPeerInterface.DestroyInstance(m_Socket);
+            return false;
+        }
+
+    }
+
+
+
+
+    public void DisConnect()
+    {
+        m_Socket.CloseConnection(m_SystemAddress, false);
+        m_Socket.Shutdown(300);
+        RakPeerInterface.DestroyInstance(m_Socket);
+        m_SystemAddress = null;
+        m_Socket = null;
+        OnDisconnected();
+
+    }
+
+    public int Send(byte[] data, int offset, int size)
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ Assets/Script/NetworkStream.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class NetworkStream
+{
+    public virtual int ReceiveBuffLength
+    {
+        get { return 1024 * 512; }
+    }
+    public virtual int SendBuffLength
+    {
+        get { return 1024 * 512; }
+    }
+    public byte[] ReceiveBuff;
+    public byte[] SendBuff;
+
+    public int ReceiveOffset;
+    public int ReadOffset;
+
+    protected int m_ReadPosition;
+    protected int m_ReadEnd;
+
+    public int SendOffset;
+
+    protected int m_WritePosition;
+    protected int m_WriteEnd;
+
+    public IConnection connection;
+    public Action m_OnMessage;
+
+    public delegate bool BufferHandle(System.IO.Stream proto);
+
+
+    public NetworkStream()
+    {
+        ReceiveBuff = new byte[ReceiveBuffLength];
+        SendBuff = new byte[SendBuffLength];
+    }
+    public void Reset()
+    {
+        ReceiveOffset = 0;
+
+        m_ReadPosition = 0;
+        m_ReadEnd = 0;
+
+        SendOffset = 0;
+
+        m_WritePosition = 0;
+        m_WriteEnd = 0;
+
+        ReadOffset = 0;
+    }
+    public void CheckWrite(int len)
+    {
+        if (len + m_WriteEnd > SendBuffLength)
+        {
+            throw new Exception(string.Format("write data to big wirte end:{0} length:{1}", m_WriteEnd, SendBuffLength));
+        }
+    }
+    public void CheckRead(int len)
+    {
+        if (len + m_ReadPosition > m_ReadEnd)
+        {
+            throw new Exception(string.Format("read data error pos:{0} length:{1} end:{2}", m_ReadPosition, len, m_ReadEnd));
+        }
+    }
+    public void BeginWrite()
+    {
+
+        m_WritePosition = SendOffset;
+        m_WriteEnd = m_WritePosition + connection.Type == ConnectionType.UDP ? 5 : 4;
+    }
+    public void EndWrite()
+    {
+        int head_len = connection.Type == ConnectionType.UDP ? 5 : 4;
+        int len = m_WriteEnd - m_WritePosition - head_len;
+        int head_index = m_WritePosition;
+        if (connection.Type == ConnectionType.UDP)
+        {
+            SendBuff[head_index++] = 254;
+        }
+        SendBuff[head_index++] = (byte)((len >> 0) & 0xff);
+        SendBuff[head_index++] = (byte)((len >> 8) & 0xff);
+        SendBuff[head_index++] = (byte)((len >> 16) & 0xff);
+        SendBuff[head_index++] = (byte)((len >> 24) & 0xff);
+        if(connection.Type==ConnectionType.TCP)
+        {
+
+        }
+        else
+        {
+            connection.Send(SendBuff, len + head_len);
+            SendOffset = 0;
+        }
+        
+
+    }
+    public void WriteData(byte[] data, int index, int size)
+    {
+        CheckWrite(size);
+        Array.Copy(data, index, SendBuff, m_WriteEnd, size);
+        m_WriteEnd += size;
+    }
+    public void WriteByte(byte data)
+    {
+        CheckWrite(1);
+        SendBuff[m_WriteEnd++] = data;
+    }
+    public void WriteInt(int data)
+    {
+        CheckWrite(4);
+        SendBuff[m_WriteEnd] = (byte)((data >> 0) & 0xff);
+        SendBuff[m_WriteEnd + 1] = (byte)((data >> 8) & 0xff);
+        SendBuff[m_WriteEnd + 2] = (byte)((data >> 16) & 0xff);
+        SendBuff[m_WriteEnd + 3] = (byte)((data >> 24) & 0xff);
+        m_WriteEnd += 4;
+    }
+    public void WriteShort(short data)
+    {
+        CheckWrite(2);
+        SendBuff[m_WriteEnd] = (byte)((data >> 0) & 0xff);
+        SendBuff[m_WriteEnd + 1] = (byte)((data >> 8) & 0xff);
+        m_WriteEnd += 2;
+    }
+    public void WriteFloat(float data)
+    {
+        CheckWrite(4);
+        byte[] bs = BitConverter.GetBytes(data);
+        Array.Copy(bs, 0, SendBuff, m_WriteEnd, bs.Length);
+        m_WriteEnd += bs.Length;
+        bs = null;
+    }
+    public void WriteVector3(Vector3 v3)
+    {
+        WriteFloat(v3.x);
+        WriteFloat(v3.y);
+        WriteFloat(v3.z);
+    }
+    public void WriteVector2(Vector2 v2)
+    {
+        WriteFloat(v2.x);
+        WriteFloat(v2.y);
+    }
+    public void WriteVector4(Vector4 v4)
+    {
+        WriteFloat(v4.x);
+        WriteFloat(v4.y);
+        WriteFloat(v4.z);
+        WriteFloat(v4.w);
+    }
+    public void WriteQuaternion(Quaternion rot)
+    {
+        WriteFloat(rot.x);
+        WriteFloat(rot.y);
+        WriteFloat(rot.z);
+        WriteFloat(rot.w);
+    }
+    public void WriteString(string str)
+    {
+        byte[] bs = Encoding.UTF8.GetBytes(str);
+        WriteInt(bs.Length);
+        WriteData(bs, 0, bs.Length);
+    }
+
+    public byte ReadByte()
+    {
+        CheckRead(1);
+        return ReceiveBuff[m_ReadPosition++];
+    }
+    public string ReadString()
+    {
+        int len = ReadInt();
+        CheckRead(len);
+        string ret = Encoding.UTF8.GetString(ReceiveBuff, m_ReadPosition, len);
+        m_ReadPosition += len;
+        return ret;
+    }
+    public float ReadFloat()
+    {
+        CheckRead(4);
+        float ret = BitConverter.ToSingle(ReceiveBuff, m_ReadPosition);
+        m_ReadPosition += 4;
+        return ret;
+    }
+    public short ReadShort()
+    {
+        CheckRead(2);
+        short ret = BitConverter.ToInt16(ReceiveBuff, m_ReadPosition);
+        m_ReadPosition += 2;
+        return ret;
+    }
+    public int ReadInt()
+    {
+        CheckRead(4);
+        int ret = BitConverter.ToInt32(ReceiveBuff, m_ReadPosition);
+        m_ReadPosition += 4;
+        return ret;
+    }
+
+
+
+
+    public Vector2 ReadVector2()
+    {
+        return new Vector2() { x = ReadFloat(), y = ReadFloat() };
+    }
+    public Vector3 ReadVecotr3()
+    {
+        return new Vector3() { x = ReadFloat(), y = ReadFloat(), z = ReadFloat() };
+    }
+    public Vector4 ReadVector4()
+    {
+        return new Vector4() { x = ReadFloat(), y = ReadFloat(), z = ReadFloat(), w = ReadFloat() };
+    }
+    public Quaternion ReadQuaternion()
+    {
+        return new Quaternion(ReadFloat(), ReadFloat(), ReadFloat(), ReadFloat());
+    }
+
+    protected virtual void OnMessage()
+    {
+        if (m_OnMessage != null) m_OnMessage();
+    }
+
+    public void OnParseMessage()
+    {
+        int revc_size = connection.Read(ReceiveBuff, ReadOffset, ReceiveBuffLength - ReadOffset);
+        if (revc_size <= 0) return;
+        ReadOffset += revc_size;
+        int size = ReadOffset - m_ReadPosition;
+        while (size > 0)
+        {
+            //小于包头
+            if (size < 4) break;
+            int data_len = BitConverter.ToInt32(ReceiveBuff, m_ReadPosition);
+            //数据未收完
+            if (size - 4 < data_len || data_len < 0) break;
+            m_ReadPosition += 4;
+            m_ReadEnd = m_ReadPosition + data_len;
+            OnMessage();
+            m_ReadPosition = m_ReadEnd;
+            size = ReadOffset - m_ReadPosition;
+        }
+        size = ReadOffset - m_ReadPosition;
+        //移动未读完的数据
+        if (m_ReadPosition > 0 && size > 0)
+        {
+            Array.Copy(ReceiveBuff, m_ReadPosition, ReceiveBuff, 0, size);
+
+        }
+        ReadOffset = size;
+        m_ReadPosition = m_ReadEnd = 0;
+
+    }
+}
